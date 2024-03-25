fd002833278ca833730a199019d01d912c28784b
Audrey-sirnik <159150103+Audrey-sirnik@users.noreply.github.com>
2024-03-25 13:30:26 +0000

    added code

--- assignment.py
+++ assignment.py
@@ -1,1 +1,12 @@
-
+import math
+print("Enter diameter here")
+diameter = input()
+if diameter.isdigit == True:
+  d = float(diameter)
+  if d > 0:
+    r = float(d/2)
+    print("results")
+    print("area is:",r**2*math.pi)
+    print("circumference is:",d*math.pi)
+else:
+  print("error")
