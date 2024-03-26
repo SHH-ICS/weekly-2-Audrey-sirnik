7ff301e40749f812502c877782cc25a3b69c3c05
Audrey-sirnik <159150103+Audrey-sirnik@users.noreply.github.com>
2024-03-26 13:04:21 +0000

    working calculator

--- assignment.py
+++ assignment.py
@@ -1,7 +1,9 @@
 import math
+print("welcome")
+print("area and circumference calulator")
 print("Enter diameter here")
 diameter = input()
-if diameter.isdigit == True:
+if diameter.isnumeric() == True:
   d = float(diameter)
   if d > 0:
     r = float(d/2)
@@ -10,3 +12,4 @@ if diameter.isdigit == True:
     print("circumference is:",d*math.pi)
 else:
   print("error")
+  print("please ensure the value you entered is a positive number without units or characters")
