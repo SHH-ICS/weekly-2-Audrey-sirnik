e453466af8bcd919795b6199b1898b6abf01ff8d
Audrey-sirnik <159150103+Audrey-sirnik@users.noreply.github.com>
2024-04-02 12:37:54 +0000

    python corrected

--- assignment.py
+++ assignment.py
@@ -3,15 +3,21 @@ print("welcome")
 print("area and circumference calulator")
 while 1:
   print("Enter diameter here")
-  diameter = float(input())
-  if diameter.isdecimal() == True:
+  diameter =input()
+  try:
     d = float(diameter)
-    r = float(d/2)
-    print("results")
-    print("area is:",r**2*math.pi)
-    print("circumference is:",d*math.pi)
-    continue
-  else:
+    if d>=0:
+      r = float(d/2)
+      print("results")
+      print("area is:",r**2*math.pi)
+      print("circumference is:",d*math.pi)
+      continue
+    else:
+      print("error")
+      print("please ensure the value you entered is a positive number without units or characters")
+      print("try again")
+      continue
+  except:
     print("error")
     print("please ensure the value you entered is a positive number without units or characters")
     print("try again")
